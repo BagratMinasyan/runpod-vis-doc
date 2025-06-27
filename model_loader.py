17f740cea0f6fd1b9e1e4ebf6d172c86fb706e6a
BagratMinasyan <bagrat.minasyan.2000@gmail.com>
2025-06-27 17:16:56 +0400

    new update

--- model_loader.py
+++ model_loader.py
@@ -1,13 +1,25 @@
 import torch
 from transformers.utils.import_utils import is_flash_attn_2_available
+from huggingface_hub import snapshot_download
 from colpali_engine.models import ColQwen2_5, ColQwen2_5_Processor
 
 def load_model():
+    local_dir = snapshot_download(
+        repo_id="Metric-AI/merged_models",
+        repo_type="model",
+        allow_patterns="exp18/*",
+        local_dir="merged_model",  
+    )
+
+    model_path = f"{local_dir}/exp18/model"
+
     model = ColQwen2_5.from_pretrained(
-        "Metric-AI/ColQwen2.5-3b-multilingual-v1.0",
+        model_path,
         torch_dtype=torch.bfloat16,
         device_map="cuda:0",
         attn_implementation="flash_attention_2" if is_flash_attn_2_available() else None,
     ).eval()
-    processor = ColQwen2_5_Processor.from_pretrained("Metric-AI/ColQwen2.5-3b-multilingual-v1.0")
+
+    processor = ColQwen2_5_Processor.from_pretrained(model_path)
+
     return model, processor
